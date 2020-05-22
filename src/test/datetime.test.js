29a0483db9bf7bb34c80f2864fa97bf2f781d411
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-05-22 16:41:44 -0300

    Introduce parameter to getFormattedDate

--- src/test/datetime.test.js
+++ src/test/datetime.test.js
@@ -3,5 +3,5 @@
 const getFormattedDate = require("../main/datetime");
 
 test("returns dd/MM/yyyy format", () => {
-  expect(getFormattedDate()).toBe("21/05/2020");
+  expect(getFormattedDate("05-21-2020")).toBe("21/05/2020");
 });
