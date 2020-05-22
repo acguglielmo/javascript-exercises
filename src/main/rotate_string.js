f38fbc524027c58c179b1c358636cbe5dd7dffa3
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-05-22 15:14:59 -0300

    Extract function

--- src/main/rotate_string.js
+++ src/main/rotate_string.js
@@ -1,12 +1,16 @@
 function rotate(string, times) {
   for (let i = 0; i < times; i++) {
-    const stringLenght = string.length;
-    const lastCharacterIndex = stringLenght - 1;
-    const lastCharacter = string.charAt(lastCharacterIndex);
-    string = lastCharacter + string.substring(0, lastCharacterIndex);
+    string = rotateOneTime(string);
   }
 
   return string;
+
+  function rotateOneTime(string) {
+    const stringLenght = string.length;
+    const lastCharacterIndex = stringLenght - 1;
+    const lastCharacter = string.charAt(lastCharacterIndex);
+    return lastCharacter + string.substring(0, lastCharacterIndex);
+  }
 }
 
 module.exports = rotate;
