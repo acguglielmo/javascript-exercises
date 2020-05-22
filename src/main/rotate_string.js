25c98d40208a125dbae5cc499b2d66a8a1a51bc0
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-05-22 14:47:07 -0300

    Introduce parameter to rotate function

--- src/main/rotate_string.js
+++ src/main/rotate_string.js
@@ -1,4 +1,10 @@
-function rotate(string) {
+function rotate(string, times) {
+  for (let i = 0; i < times; i++) {
+    let stringLenght = string.length;
+    let lastCharacter = string.charAt(stringLenght - 1);
+    string = lastCharacter + string.substring(0, stringLenght - 1);
+  }
+
   return string;
 }
 
--- src/test/rotate_string.test.js
+++ src/test/rotate_string.test.js
@@ -3,5 +3,9 @@
 const rotate = require("../main/rotate_string");
 
 test("returns rotated string 0 times", () => {
-  expect(rotate("w3resource")).toBe("w3resource");
+  expect(rotate("w3resource", 0)).toBe("w3resource");
+});
+
+test("returns rotated string 1 times", () => {
+  expect(rotate("w3resource", 1)).toBe("ew3resourc");
 });
