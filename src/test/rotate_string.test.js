4c7893e796578e70a669027744b7a103c043e2df
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-05-22 14:51:38 -0300

    Add more tests to rotate function

--- src/test/rotate_string.test.js
+++ src/test/rotate_string.test.js
@@ -6,6 +6,14 @@ test("returns rotated string 0 times", () => {
   expect(rotate("w3resource", 0)).toBe("w3resource");
 });
 
-test("returns rotated string 1 times", () => {
+test("returns rotated string 1 time", () => {
   expect(rotate("w3resource", 1)).toBe("ew3resourc");
+});
+
+test("returns rotated string 2 times", () => {
+  expect(rotate("w3resource", 2)).toBe("cew3resour");
+});
+
+test("returns rotated string 1 time 2 times sequentially", () => {
+  expect(rotate(rotate("w3resource", 1), 1)).toBe("cew3resour");
 });
