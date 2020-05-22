0748239b3b846ea5e902a7800691e9133ae4890f
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-05-22 15:10:44 -0300

    Change local variables to const

--- src/main/rotate_string.js
+++ src/main/rotate_string.js
@@ -1,8 +1,8 @@
 function rotate(string, times) {
   for (let i = 0; i < times; i++) {
-    let stringLenght = string.length;
+    const stringLenght = string.length;
     const lastCharacterIndex = stringLenght - 1;
-    let lastCharacter = string.charAt(lastCharacterIndex);
+    const lastCharacter = string.charAt(lastCharacterIndex);
     string = lastCharacter + string.substring(0, lastCharacterIndex);
   }
 
